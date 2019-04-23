cdd5f4a7603b2fc12ac624e2e1417593d3fd9d51
mechiventocilla87 <mercedesventocilla@gmail.com>
2019-04-23 20:47:14 -0300

    changes

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-
-
-//to add tasks
-
-$('#boton').on('click', function (){
+function addTask(){
     var ul = $('#ul');
     var valorInput = $('#input_text').val();
     var span = '<span>'+valorInput +'</span> <br> ';
@@ -10,19 +6,31 @@ $('#boton').on('click', function (){
     var btnGuardar= "<button class='guardar hidden'>Save</button>";
     var btnEditar = "<button class='editar'>Edit</button>";
     var btnBorrar = "<button class='borrar'>Trash</button>";
-    
-      if (valorInput != '') {
-      ul.append('<li class="li_add">'+span+input+btnGuardar+btnEditar+btnBorrar+'<hr class="hr_add"></li>');
-      $('.totales').html('Cantidad de tareas creadas: '+ contador());
-    
+
+    if (valorInput != '') {
+    ul.append('<li class="li_add">'+span+input+btnGuardar+btnEditar+btnBorrar+'<hr class="hr_add"></li>');
+    $('.totales').html('Cantidad de tareas creadas: '+ contador());
     }
 
+    $('#input_text').val('');
+}
+ 
+
+//to add tasks pushing button
+$('#boton').on('click', function (){
+    addTask();
 })
 
 
+$('#input_text').on('keypress', function(e){
+    
+    if(e.keyCode===13){
+       addTask();
+    }
+})
+
 
 //counter of <li>
-
 function contador(){
     var cantidadDeLi = $('li').length;
     return cantidadDeLi
@@ -36,16 +44,12 @@ function contadorTach_(){
 }
 
 
-
-
-
 //to delete task
 $(document).on('click', '.borrar', function () { 
     $(this).parent().remove();
     $('.totales').html('Cantidad de tareas creadas: '+ contador());
     $('.finalizadas').html('Cantidad de tareas finalizadas: '+ contadorTach_() +'  ');
 })
-
 
 
 //to edit task
@@ -58,8 +62,6 @@ $(document).on('click', '.editar', function () {
     $(this).siblings('.guardar').removeClass('hidden');
     $(this).siblings('span').addClass('hidden');
 });
-
-
 
 
 //task complete
@@ -89,9 +91,9 @@ $(document).on('click', '.guardar', function(){
 });
 
 
-//to save doing enter
-$(document).on('keypress', 'li .elInput', function(event) {
-    if (event.keyCode === 13) {
+//to save doing enter in task edited
+$(document).on('keypress', 'li .elInput', function(e) {
+    if (e.keyCode === 13) {
         var nuevaTarea = $(this).val();
         $(this).siblings('span').html(nuevaTarea);
         $(this).siblings("span").removeClass("hidden");
